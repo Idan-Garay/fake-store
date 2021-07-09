65977f230f8083e44bff1b772737a20d12609451
Idan-Garay <garayidan@gmail.com>
2021-07-09 13:31:44 +0800

    react-refresh import

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,7 +1,11 @@
 const webpack = require('webpack')
+const webpack = require('@pmmwh/react-refresh-webpack-plugin')
 
 module.exports = {
     mode: 'development',
+    devServer: {
+        hot: true,
+    },
     devtool: 'cheap-module-source-map',
     plugins: [
         new webpack.DefinePlugin({

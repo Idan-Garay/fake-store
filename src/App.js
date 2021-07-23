8a5832e424fda3208d0374f23b54cec69d5d37ca
Idan-Garay <garayidan@gmail.com>
2021-07-24 01:53:09 +0800

    search title

--- src/App.js
+++ src/App.js
@@ -8,16 +8,30 @@ const App = () => {
   const [products, setProducts] = useState([])
   const [categories, setCategories] = useState([])
   const [search, setSearch] = useState("")
+  const [filter, setFilter] = useState({})
+  const [result, setResult] = useState([])
 
   const handleChange = (e) => {
     setSearch(e.target.value)
-    console.log(search)
+  }
+  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(e)
+    setResult(
+      products.filter(data => {
+        return data.title.search(search) != -1
+      })
+    )
   }
 
   useEffect(() => {
     fetch('https://fakestoreapi.com/products?limit=3')
       .then(res=>res.json())
-      .then(setProducts)
+      .then(json => {
+        setProducts(json)
+        setResult(json)
+      })
 
       console.log(products)
       if (products.length > 0)
@@ -27,14 +41,19 @@ const App = () => {
   useEffect(() => {
     fetch('https://fakestoreapi.com/products/categories')
       .then(res=>res.json())
-      .then(setCategories)
-      
+      .then(json => {
+        setCategories(json)
+        json.map(data => {setFilter({[data]: false})})
+      })
+      console.log(filter)
   }, [categories.length])
 
   return (
     <div className="px-4 sm:px-6 lg:px-4 xl:px-6 pt-4 pb-4 sm:pb-6 lg:pb-4 xl:pb-6 space-y-4 border-dotted">
-      {(categories.length? <SearchParams categories={categories} handleChange={handleChange} />: "...loading")}
-      <Results loaded={loaded} products={products} />
+      {(categories.length? 
+        <SearchParams categories={categories} setFilter={setFilter} handleChange={handleChange} handleSubmit={handleSubmit} filter={filter}/>
+        : "...loading")}
+      <Results loaded={loaded} products={result} />
     </div>
   )
 }
--- src/components/SearchParams.js
+++ src/components/SearchParams.js
@@ -1,9 +1,11 @@
 
 const SearchParams = (props) => {
-  const {categories, handleChange} = props
+  const {categories, handleChange, handleSubmit, filter, setFilter} = props
 
   return (
-    <form style={{border: "1px solid black"}} className="w-1/5 p-1">
+    <form 
+      onSubmit={handleSubmit}
+      style={{border: "1px solid black"}} className="w-1/5 p-1">
       <div className="search grid grid-cols-3 gap-4" >
         <input className="col-span-2" type="text" onChange={handleChange} placeholder="item search" style={{border: "1px solid black"}}/>
         <button className="col-span-1" style={{border: "1px solid black"}}>Submit</button>
@@ -12,7 +14,7 @@ const SearchParams = (props) => {
         {
           categories.map((category, i) => (
           <label htmlFor={category} className="w-full" key={`category-${i}`} >
-            <input type="checkbox" value={category} id={category} className="mx-5" />
+            <input type="checkbox" id={category} className="mx-5" value={filter[category]} onChange={() => setFilter( {[category]: !filter[category]} )} />
             {category}
           </label>)
           )

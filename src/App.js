42e723b4575c84a300ee4debbb65afca97162b67
Idan-Garay <garayidan@gmail.com>
2021-07-28 21:26:11 +0800

    err & navigation

--- src/App.js
+++ src/App.js
@@ -1,58 +1,94 @@
-import {React, StrictMode, useState, useEffect} from 'react'
+import {React, StrictMode, useState, useEffect, useRef} from 'react'
 import ReactDOM from 'react-dom'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
 import Results from './components/Results'
-// import SearchParams from './components/SearchParams'
 import Categories from './components/Categories'
+import {ErrorBoundary} from 'react-error-boundary'
+
 
 const App = () => {
   const [loaded, setLoaded] = useState(false)
   const [products, setProducts] = useState([])
-  const [categories, setCategories] = useState([])
-  const [search, setSearch] = useState("")
-  const [filter, setFilter] = useState({})
-  const [result, setResult] = useState([])
+  const [category, setCategory] = useState('all')
+  const prevCategoryRef = useRef(category)
 
-  const handleChange = (e) => {
-    setSearch(e.target.value)
+  const handleClick = e => {
+    setCategory(e.target.value.toString().toLowerCase())
+    setLoaded(false)
   }
-  
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(e)
-    setResult(
-      products.filter(data => {
-        return data.title.search(search) != -1
-      })
+
+  const ErrorFallback = ({error}) => {
+    return (
+      <div>
+        <p>Something went wrong:</p>
+        <pre>error.message</pre>
+      </div>
     )
   }
+  
+  useEffect(() => {
+    console.log(category)
+    prevCategoryRef.current = category
+    console.log(prevCategoryRef.current)
+  }, [category])
+
+  const prevCategory = prevCategoryRef.current
 
   useEffect(() => {
-    fetch('https://fakestoreapi.com/products?limit=3')
-      .then(res=>res.json())
-      .then(json => {
-        setProducts(json)
-        setResult(json)
-      })
-
-      console.log(products)
-      if (products.length > 0)
-        setLoaded(true)
-  }, [products.length])
-
-  useEffect(() => {
-    fetch('https://fakestoreapi.com/products/categories')
-      .then(res=>res.json())
-      .then(json => {
-        setCategories(json)
-        json.map(data => {setFilter({[data]: false})})
-      })
-      console.log(filter)
-  }, [categories.length])
+    // let val = category === 'all' ? 'categories' : `category/${category}`
+    if (!products.length || ( prevCategory !== category)) {
+      const url = `https://fakestoreapi.com/products/${category === 'all' ? '': `category/${category}` }`
+      fetch(url)
+        .then(res => {
+          const retVal = res.json()
+          console.log(res, url)
+          console.log(retVal)
+          return retVal
+        }, error => console.log(error))
+        .then(json => {
+          setProducts(json)
+          setLoaded(true)
+        }, error => console.log(error))
+    }
+  }, [category])
 
   return (
     <div className="px-4 sm:px-6 lg:px-4 xl:px-6 pt-4 pb-4 sm:pb-6 lg:pb-4 xl:pb-6 space-y-4 border-dotted">
-      <Categories />
-      <Results loaded={loaded} products={result} />
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <Router>
+        <nav>
+          <ul className="flex">
+            <li className="mr-3">
+              <Link to="/" className="inline-block border border-blue-500 rounded py-1 px-3 bg-blue-500 text-white" >Home</Link>
+            </li>
+            <li className="mr-3">
+              <Link to="/store" className="inline-block border border-blue-500 rounded py-1 px-3 bg-blue-500 text-white" >Store</Link>
+            </li>
+            <li className="mr-3">
+              <Link to="/user" className="inline-block border border-blue-500 rounded py-1 px-3 bg-blue-500 text-white" >User</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/store">
+            <Categories handleClick={handleClick}/>
+            <Results loaded={loaded} products={products} />
+          </Route>
+          <Route path="/user">
+            <h1>This is user page</h1>
+          </Route>
+          <Route path="/">
+            <h1>This is Home page</h1>
+          </Route>
+        </Switch>
+      </Router>
+    </ErrorBoundary>
     </div>
   )
 }
--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,11 +1,11 @@
 
-const Categories = () => {
+const Categories = ({handleClick}) => {
   return (
     <div className="space-x-2">
-      <button className="button">Electronics</button>
-      <button className="button">Jewelry`</button>
-      <button className="button">Men Clothing</button>
-      <button className="button">Women Clothing</button>
+      <button onClick={handleClick} value="Electronics" className="shadow p-2 button w-2/12">Electronics</button>
+      <button onClick={handleClick} value="jewelery" className="shadow p-2 button w-2/12">Jewelry</button>
+      <button onClick={handleClick} value="Men's Clothing" className="shadow p-2 button w-2/12">Men Clothing</button>
+      <button onClick={handleClick} value="Women's Clothing" className="shadow p-2 button w-2/12">Women Clothing</button>
     </div>
   )
 }

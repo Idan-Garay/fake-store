d8fcbe0a8ab354acf2526834ce0c4baf590f14f3
Idan-Garay <garayidan@gmail.com>
2021-07-30 15:03:34 +0800

    center categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,7 +1,7 @@
 
 const Categories = ({handleClick}) => {
   return (
-    <div className="space-x-2">
+    <div className="space-x-2 flex justify-center">
       <button onClick={handleClick} value="Electronics" className="shadow p-2 button w-2/12">Electronics</button>
       <button onClick={handleClick} value="jewelery" className="shadow p-2 button w-2/12">Jewelry</button>
       <button onClick={handleClick} value="Men's Clothing" className="shadow p-2 button w-2/12">Men Clothing</button>

9be42fd39ab8e8fbe2f1279b71a347f941e27ba9
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2022-06-19 07:10:18 +0800

    connect product to product detail [pid].jsx]

--- components/product.jsx
+++ components/product.jsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Product(props) {
+  const pid = props.id;
   const pName =
     props.title || "Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport";
   const price = props.price || "$599";
@@ -15,28 +17,30 @@ export default function Product(props) {
           src={pImg}
           layout="fill"
         />
-        <div className="opacity-0 hover:opacity-100 hover:bg-zinc-600/25 duration-300 absolute inset-0 z-10 flex justify-center items-end">
-          <div className="bg-black h-1/4 w-full text-white flex-col">
-            <h4 className="h-2/5 truncate">{pName}</h4>
-            <div className="flex flex-nowrap justify-between h-1/3">
-              <h6>${price}</h6>
-              <div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-full w-full"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z"
-                    clipRule="evenodd"
-                  />
-                </svg>
+        <Link href={`store/${pid}`}>
+          <div className="cursor-pointer opacity-0 hover:opacity-100 hover:bg-zinc-600/25 duration-300 absolute inset-0 z-10 flex justify-center items-end">
+            <div className="bg-black h-1/4 w-full text-white flex-col">
+              <h4 className="h-2/5 truncate">{pName}</h4>
+              <div className="flex flex-nowrap justify-between h-1/3">
+                <h6>${price}</h6>
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-full w-full"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </Link>
       </div>
     </div>
   );

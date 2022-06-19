c5585a7c30cb8553c6d62c657f1feca9c3f2beda
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2022-06-19 20:27:12 +0800

    removed unnecessary lines

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,11 @@
 import { useState, useCallback } from "react";
-import useSWR from "swr";
-import Categories from "../components/categories";
 import ProductGallery from "../components/productGallery";
 import Tabs from "../components/tabs";
 
 const Home = () => {
   const [category, setCategory] = useState("all");
   const handleTabClick = useCallback((cat) => setCategory(cat), []);
+
   return (
     <div className="flex flex-col min-h-screen justify-center px-2 ">
       <Tabs category={category} handleClick={handleTabClick} />

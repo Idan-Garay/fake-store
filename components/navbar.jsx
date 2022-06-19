5a5bb91009efd0e97462b35bc3b65763e931c62f
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2022-06-19 10:32:46 +0800

    cart badge

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -11,10 +11,15 @@ export default function () {
             </span>
           </Link>
         </div>
-        <div className="py-6 px-5 space-y-6">
+        <div className="py-5 px-5 space-y-6">
           <div className="border-black border-2 flex items-center space-x-4">
             <Link href="/carts">
-              <a className="hover:text-gray-300">Cart</a>
+              <div className="flex gap-2">
+                <a className="hover:text-gray-300">Cart</a>
+                <span class="inline-flex items-center justify-center p-2 mr-2 text-xs font-bold leading-none text-black bg-white rounded-full">
+                  9
+                </span>
+              </div>
             </Link>
             <Link href="/">
               <a className="text-indigo-600 hover:text-indigo-500">Products</a>

fab4b14d7d6f94659768077ae41ad5e3b820da9f
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2022-06-19 10:18:25 +0800

    Cart Receipt

--- pages/carts/index.jsx
+++ pages/carts/index.jsx
@@ -26,9 +26,24 @@ export default function Index() {
           <CartProduct />
           <CartProduct />
         </div>
-        <div className="total border-y h-2/6">
-          <div></div>
-          <div></div>
+        <div className="total border-y h-2/6 py-4 flex flex-row flex-nowrap justify-between">
+          <div className="w-1/2 px-2 py-1"></div>
+          <div className="w-1/2  px-2 py-1 flex justify-end">
+            <ul className="w-5/6  ">
+              <li className="flex flex-row flex-nowrap justify-between">
+                <span className="text-zinc-400">Total qty. : </span>
+                <span>20</span>
+              </li>
+              <li className="flex flex-row flex-nowrap justify-between">
+                <span className="text-zinc-400">Unique items: </span>
+                <span>20</span>
+              </li>
+              <li className="flex flex-row flex-nowrap justify-between text-lg mt-1">
+                <span className="">Total: </span>
+                <span className="font-semibold">20 </span>
+              </li>
+            </ul>
+          </div>
         </div>
         <div className="buttons h-1/6 flex flex-row flex-nowrap justify-end items-center">
           <button className="border border-indigo-300/25 px-5 py-2 hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">

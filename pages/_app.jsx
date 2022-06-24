849c3411877d4f31908581cac92bcae435c81932
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2022-06-24 10:03:28 +0800

    globalized products and carts with custom hooks

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,10 @@
 import "../styles/globals.css";
 import Layout from "../components/layout";
+import { useCarts, useProducts } from "./api/useHooks";
 
 function MyApp({ Component, pageProps }) {
+  global.store = useCarts();
+  global.storeProducts = useProducts();
   return (
     <Layout>
       <Component {...pageProps} />

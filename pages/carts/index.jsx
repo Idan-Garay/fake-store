0342a62c98edab28d527fceff32a66bdb1f60585
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2022-06-24 10:04:41 +0800

    added change cart logic

--- pages/carts/index.jsx
+++ pages/carts/index.jsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
 
 import Cart from "../../components/cart";
-import { useCarts } from "../api/useHooks";
+import CartProduct from "../../components/cartProduct";
 
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
-
-export function CartPaginate({ carts }) {
+export function CartPaginate({ carts, setCartId }) {
   return (
     <>
       {carts.map((cart) => (
-        <Cart key={`cart-${cart.id}`} {...cart} />
+        <Cart
+          key={`cart-${cart.id}`}
+          {...cart}
+          onClick={() => setCartId(cart.id)}
+        />
       ))}
     </>
   );
@@ -17,12 +19,13 @@ export function CartPaginate({ carts }) {
 
 export default function Index() {
   const [index, setIndex] = useState(3);
-  const { carts, isLoading, isError } = useCarts();
+  const { carts, cartId, isLoading, setCartId, isError } = global.store;
+  const products = global.storeProducts.products;
 
   const handlePagination = (isNext = false) => {
     let val = index;
 
-    if (isNext && val - cartData.length < 2) val += 3;
+    if (isNext && val - carts.length < 2) val += 3;
     else if (!isNext && val - 3 >= 3) val -= 3;
     setIndex(val);
   };
@@ -35,7 +38,10 @@ export default function Index() {
         <h1 className=" font-medium text-xl mb-4">Selected Cart:</h1>
         <div className="flex flex-col items-center h-full w-5/6">
           <div className="h-3/6 flex flex-col gap-2 ">
-            <CartPaginate carts={carts.slice(index - 3, index)} />
+            <CartPaginate
+              carts={carts.slice(index - 3, index)}
+              setCartId={setCartId}
+            />
           </div>
           <div className="flex flex-row flex-nowrap gap-2 justify-center mt-5">
             <button
@@ -64,9 +70,13 @@ export default function Index() {
           </button>
         </div>
         <div className=" h-3/6 flex flex-row flex-nowrap gap-4 justify-start py-2">
-          {/* {data.map((prod) => (
-            <CartProduct key={"cartProd-" + prod.id} {...prod} />
-          ))} */}
+          {products &&
+            carts[cartId].products.map((prod) => (
+              <CartProduct
+                key={"cartProd-" + prod.productId}
+                {...products[parseInt(prod.productId) - 1]}
+              />
+            ))}
         </div>
         <div className="total border-y h-2/6 py-4 flex flex-row flex-nowrap justify-between">
           <div className="w-1/2 px-2 py-1"></div>
@@ -75,25 +85,27 @@ export default function Index() {
               <li className="flex flex-row flex-nowrap justify-between">
                 <span className="text-zinc-400">Total qty. : </span>
                 <span>
-                  {/* {cart.products.reduce(
+                  {carts[cartId].products.reduce(
                     (prev, curr) => parseInt(curr.quantity) + prev,
                     0
-                  )} */}
+                  )}
                 </span>
               </li>
               <li className="flex flex-row flex-nowrap justify-between">
                 <span className="text-zinc-400">Unique items: </span>
-                {/* <span>{cart.products.length}</span> */}
+                <span>{carts[cartId].products.length}</span>
               </li>
               <li className="flex flex-row flex-nowrap justify-between text-lg mt-1">
                 <span className="">Total: </span>
                 <span className="font-light">
-                  {/* ${" "}
-                  {cart.products.reduce(
-                    (prev, curr, idx) =>
-                      data[idx].price * parseInt(curr.quantity) + prev,
-                    0
-                  )} */}
+                  {products &&
+                    carts[cartId].products.reduce(
+                      (prev, curr) =>
+                        products[parseInt(curr.productId) - 1].price *
+                          parseInt(curr.quantity) +
+                        prev,
+                      0
+                    )}
                 </span>
               </li>
             </ul>

6c03db00ad443fe4be185e9393666ec261794677
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2023-03-27 21:38:08 +0800

    fix: add layout for cart page

--- react_useContext_useReducer/src/cart/index.tsx
+++ react_useContext_useReducer/src/cart/index.tsx
@@ -2,7 +2,30 @@ import React from 'react'
 
 const CartPage = () => {
   return (
-    <div>Cart</div>
+    <main className="h-full w-full border-4 flex px-4 gap-x-6">
+      <section className="left_products w-8/12 border border-red-600 flex flex-col ">
+        <div className="my_cart h-[5rem] border">My Cart</div>
+
+        <div className="choose_cart h-[3rem] border">
+          <input type="checkbox" name="" id="" />Choose All Product
+        </div>
+
+        <div className="products h-[15rem] border"></div>
+      </section>
+
+      <section className="right_invoice self-start flex flex-col w-4/12 h-5/6 border border-blue-600">
+        <div className="border h-2/6">Order Summary</div>
+        <div className="border h-1/6">Delivery Today with</div>
+        <div className="border h-1/6">Amount + Tax</div>
+        <div className="border h-2/6 flex flex-col">
+          <div className="border h-1/3">Order Total</div>
+          <div className="border h-2/3 flex flex-col  place-content-end gap-y-3">
+            <button>Add Coupon Code Here</button>
+            <button>Checkout</button>
+          </div>
+        </div>
+      </section>
+    </main>
   )
 }
 

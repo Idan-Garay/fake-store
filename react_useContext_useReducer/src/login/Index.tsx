4f9ea0543b9c6cb1579c01bb3645512267533ef0
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2023-03-30 15:58:10 +0800

    fix: add onChange setState for login inputs

--- react_useContext_useReducer/src/login/Index.tsx
+++ react_useContext_useReducer/src/login/Index.tsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const LoginPage = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+
     return (
         <main className='w-full h-[100vh] border flex flex-col items-center justify-center flex-grow-0'>
             <div className=" h-4/6 w-5/12 py-12 px-24 rounded border flex flex-col gap-y-6">
                 <h1 className='text-3xl font-semibold text-center'>Fake Z</h1>
                 <div className="input-group flex flex-col">
                     <label htmlFor="email">Email</label>
-                    <input type="email" name="email" id="email" className='border rounded h-9 p-2' />
+                    <input type="email" onChange={e => setEmail(e.target.value)} defaultValue={email} name="email" id="email" className='border rounded h-9 p-2' />
                 </div>
                 <div className="input-group flex flex-col">
                     <label htmlFor="password">Password</label>
-                    <input type="password" name="password" id="password" className='border rounded h-9 p-2' />
+                    <input type="password" onChange={e => setPassword(e.target.value)} defaultValue={password} name="password" id="password" className='border rounded h-9 p-2' />
                 </div>
                 <div className="input-group flex items-center gap-x-3">
                     <input type="checkbox" name="remember me" id="remember_me" className='border rounded h-9 p-2' />

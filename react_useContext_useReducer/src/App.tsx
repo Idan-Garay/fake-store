5afe257a434b39fdac6f6e8522d827d1486c5174
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2023-04-02 11:22:58 +0800

    fix: transfer AuthAccount Context to App

--- react_useContext_useReducer/src/App.tsx
+++ react_useContext_useReducer/src/App.tsx
@@ -1,18 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
 import {
   Routes,
   Route,
+  useNavigate,
 } from 'react-router-dom';
 import CartPage from './cart/Index';
 import IndexPage from './Index';
 import LoginPage from './login/Index';
 import IndexLayout from './IndexLayout';
+import { AuthAccountContext, AuthAccountDispatchContext, useAuthAccount } from '../context/AuthContext';
 
 function App() {
   // necessary useContext for sharing state between pages when redirecting page via url input
- 
+  const [authAccountState, authAccountDispatch] = useAuthAccount()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!authAccountState.authenticated) {
+      navigate('/login')
+    }
+  }, [authAccountState.authenticated])
+
   return (
+
+    <AuthAccountContext.Provider value={authAccountState}>
+      <AuthAccountDispatchContext.Provider value={authAccountDispatch}>
         <div className="App min-h-[100vh] w-[100vw] flex flex-col">
           <Routes>
             <Route path='/' element={<IndexLayout />}>
@@ -23,6 +36,9 @@ function App() {
             <Route path='/login' index element={<LoginPage />} />
           </Routes>
         </div>
+
+      </AuthAccountDispatchContext.Provider>
+    </AuthAccountContext.Provider>
   )
 }
 
--- react_useContext_useReducer/src/IndexLayout.tsx
+++ react_useContext_useReducer/src/IndexLayout.tsx
@@ -7,20 +7,15 @@ import Navbar from './navbar'
 
 const IndexLayout = () => {
     const [cartState, dispatch] = useCart()
-    const [authAccountState, authAccountDispatch] = useAuthAccount()
 
     return (
         <>
-            <AuthAccountContext.Provider value={authAccountState}>
-                <AuthAccountDispatchContext.Provider value={authAccountDispatch}>
-                    <CartContext.Provider value={cartState}>
-                        <CartDispatchContext.Provider value={dispatch}>
-                            <Navbar />
-                            <Outlet />
-                        </CartDispatchContext.Provider>
-                    </CartContext.Provider>
-                </AuthAccountDispatchContext.Provider>
-            </AuthAccountContext.Provider>
+            <CartContext.Provider value={cartState}>
+                <CartDispatchContext.Provider value={dispatch}>
+                    <Navbar />
+                    <Outlet />
+                </CartDispatchContext.Provider>
+            </CartContext.Provider>
         </>
     )
 }
